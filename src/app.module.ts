63ddf4495edc64f01d5fa7ff418b172868235205
danielmlc <kidbeibei1988@126.com>
2018-10-11 00:53:10 +0800

    frist commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CatModule } from 'cat/cat.module';
+import { ErrorsInterceptor } from 'common/errors.interceptor';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(), CatModule],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorsInterceptor
+    },
+    AppService],
+})
+
+export class AppModule {}
